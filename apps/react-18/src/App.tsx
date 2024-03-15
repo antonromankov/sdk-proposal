d5e7324bdd297572fb8228c120594591eccaae3d
Anton Romankov <anton.romankov@promwad.com>
2024-03-15 17:18:08 +0100

    Add auth to react18 app

--- apps/react-18/src/App.tsx
+++ apps/react-18/src/App.tsx
@@ -1,3 +1,20 @@
+import { useAuth } from '@aroma/sdk-react-17'
+
 export const App = () => {
-	return <div>React 18 App</div>
+	const { isAuthenticated, logout, login } = useAuth()
+
+	const buttonText = isAuthenticated ? 'Logout' : 'Login'
+
+	const onClick = () => {
+		isAuthenticated ? logout() : login()
+	}
+
+	return (
+		<div>
+			<h1>React 18 App</h1>
+			<button type="button" onClick={onClick}>
+				{buttonText}
+			</button>
+		</div>
+	)
 }
